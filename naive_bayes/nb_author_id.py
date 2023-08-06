cba1d125d850320f897aa7be48f971a7be8716fd
AndyGyasi <gyasiandy1@gmail.com>
2023-08-06 22:00:05 +0000

    made predictions using an svm

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -22,9 +22,6 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-##############################################################
-# Enter Your Code Here
-
 from sklearn.naive_bayes import GaussianNB
 
 ### create classifier
@@ -45,21 +42,3 @@ from sklearn.metrics import accuracy_score
 
 accuracy = accuracy_score(pred, labels_test)
 print(accuracy)
-##############################################################
-
-##############################################################
-'''
-You Will be Required to record time for Training and Predicting 
-The Code Given on Udacity Website is in Python-2
-The Following Code is Python-3 version of the same code
-'''
-
-# t0 = time()
-# # < your clf.fit() line of code >
-# print("Training Time:", round(time()-t0, 3), "s")
-
-# t0 = time()
-# # < your clf.predict() line of code >
-# print("Predicting Time:", round(time()-t0, 3), "s")
-
-##############################################################
