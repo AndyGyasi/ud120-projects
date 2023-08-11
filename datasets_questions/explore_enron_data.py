b6102dbc56ddef472e57da5aa1d0dd0d48b680d4
AndyGyasi <gyasiandy1@gmail.com>
2023-08-11 16:24:11 +0000

    finding quantified values in the enron data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -22,6 +22,11 @@ import joblib
 
 enron_data = joblib.load(open("../final_project/final_project_dataset_unix.pkl", "rb"))
 
+# finding the first value
+for first in enron_data.values():
+    print(first)
+    break
+
 # finding the size of the dataset
 print(len(enron_data))
 print(enron_data.items())
@@ -47,4 +52,56 @@ print(enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
 # finding the stock options of Jeffrey K Skilling
 print(enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
 
-print(enron_data)
+# finding the maximum total payments
+payments = {}
+for person, value in enron_data.items():
+    payment = value["total_payments"]
+    name = person
+    if payment == 'NaN':
+        payment = 0
+    payments[name] = payment
+
+for payment in payments.values():
+    if payment == max(payments.values()):
+        print(payment)
+
+# finding quantified salaries
+count_salary = 0
+count_no_salary = 0
+for value in enron_data.values():
+    salary = value["salary"]
+    if salary == 'NaN':
+        count_no_salary =+ 1
+    else:
+        count_salary += 1
+
+print(count_salary, count_no_salary)
+
+# finding quantified salaries
+count_email = 0
+count_no_email = 0
+for value in enron_data.values():
+    email = value["email_address"]
+    if email == 'NaN':
+        count_no_email =+ 1
+    else:
+        count_email += 1
+
+# function is not working
+"""
+print(count_email, count_no_email)
+def quantified_value(values_dict, value_key):
+    count_value = 0
+    count_no_value = 0
+    for values in values_dict:
+        value = values[value_key]
+        if value == 'Nan':
+            count_no_value += 1
+        else:
+            count_value += 1
+    return count_value, count_no_value
+
+quantified_value(enron_data.values(), "salary")    
+"""
+
+
