62993f6255b2021f45cd5c20d63bb74a12a27026
AndyGyasi <gyasiandy1@gmail.com>
2023-08-15 10:07:07 +0000

    created a regressor for the enron's finance data

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,13 +17,13 @@ import sys
 import joblib
 sys.path.append(os.path.abspath("../tools/"))
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = joblib.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
+dictionary = joblib.load( open("../final_project/final_project_dataset_modified_unix.pkl", "rb") )
 
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys_unix.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
@@ -39,11 +39,28 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+# import regressor
+from sklearn.linear_model import LinearRegression
 
+# create regressor
+reg = LinearRegression()
 
+# fit regressor
+reg = reg.fit("bonus", "salary")    
 
+# finding the slope
+reg_coef = reg.coef_
 
+# finding the intercept
+reg_intercept = reg.intercept_
 
+# finding the rsquared
+rsquared = reg.score("bonus", "salary")
+
+# predicting Katie's net worh
+#pred = reg.predict([27])
+ 
+print(reg, reg_coef, reg_intercept, rsquared) 
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -56,7 +73,6 @@ for feature, target in zip(feature_train, target_train):
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
-
 
 
 
